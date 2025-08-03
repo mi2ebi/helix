e6bdc125641511a9b06f64c56132b262baf2c07d
Ithai Levi <randomlinesofcode@gmail.com>
2025-08-03 11:04:00 +0100

    Adding enable-focus-dimmer config to dim text in unfocused views or while jumping to labels

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -385,6 +385,8 @@ pub struct Config {
     pub enable_focus_dimmer: bool,
     /// Whether to enable Kitty Keyboard Protocol
     pub kitty_keyboard_protocol: KittyKeyboardProtocolConfig,
+    /// Whether to dim text in unfocused views or while jumping to labels.
+    pub enable_focus_dimmer: bool,
 }
 
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize, Clone, Copy)]
@@ -1076,6 +1078,7 @@ impl Default for Config {
             rainbow_brackets: false,
             enable_focus_dimmer: false,
             kitty_keyboard_protocol: Default::default(),
+            enable_focus_dimmer: false,
         }
     }
 }
