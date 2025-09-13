abe82fec04e657ff3bf86b74c1ecd025cc63afc4
mi2ebi <merrybot47@gmail.com>
2025-09-13 13:17:54 -0700

    fix duplicate config keys

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -385,10 +385,6 @@ pub struct Config {
     pub enable_focus_dimmer: bool,
     /// Whether to enable Kitty Keyboard Protocol
     pub kitty_keyboard_protocol: KittyKeyboardProtocolConfig,
-    /// Whether to dim text in unfocused views or while jumping to labels.
-    pub enable_focus_dimmer: bool,
-    /// Whether to enable Kitty Keyboard Protocol
-    pub kitty_keyboard_protocol: KittyKeyboardProtocolConfig,
 }
 
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize, Clone, Copy)]
@@ -1087,8 +1083,6 @@ impl Default for Config {
             clipboard_provider: ClipboardProvider::default(),
             editor_config: true,
             rainbow_brackets: false,
-            enable_focus_dimmer: false,
-            kitty_keyboard_protocol: Default::default(),
             enable_focus_dimmer: false,
             kitty_keyboard_protocol: Default::default(),
         }
