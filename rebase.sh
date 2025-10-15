894539b17012ca694e923baf3f93f68d512ee227
mi2ebi <merrybot47@gmail.com>
2025-10-15 11:46:15 -0700

    [eviehelix] autorebase local stuff too

--- rebase.sh
+++ rebase.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-PRS=(13430 13492 13650 13760 14073 14121 14151)
+PRS=(13430 13492 13650 13736 13760 14072 14121 14151)
+LOCALS=(evie-tweaks)
 git fetch upstream
 for pr in "${PRS[@]}"; do
   branch=$(git branch --list "pr$pr-*" | head -n1 | xargs)
   echo "rebasing $branch"
   git rebase upstream/master "$branch" || {
-    echo "conflict in $branch, fix it and run `git rebase --continue`"
+    echo "conflict in $branch, fix it and run 'git rebase --continue', then rerun this script"
+    exit 1
+  }
+done
+for local in "${LOCALS[@]}"; do
+  echo "rebasing $local"
+  git rebase upstream/master "$local" || {
+    echo "conflict in $local, fix it and run 'git rebase --continue', then rerun this script"
     exit 1
   }
 done
 echo "rebuilding master"
 git checkout master
 git reset --hard upstream/master
-
 for pr in "${PRS[@]}"; do
   branch=$(git branch --list "pr$pr-*" | head -n1 | xargs)
   git merge "$branch" --no-edit
+done
+for local in "${LOCALS[@]}"; do
+  git merge "$local" --no-edit
 done
